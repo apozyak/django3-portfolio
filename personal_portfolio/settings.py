659733f86cf5f5abc9f8b24131bc6bf69671ea61
Andrey Pozyak <apozyak@yandex.ru>
2022-10-15 10:04:23 +0300

    new two files

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -120,3 +120,9 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+try:
+    from .local_settings import *
+except ImportError:
+    print('нет локального файла, скорей всего работа в продакшане')
+
